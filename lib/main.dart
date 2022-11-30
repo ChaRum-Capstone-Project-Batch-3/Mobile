30786ab191cde1434b4ba71c847242320e985bd5
masnurrm <nurmuhammad448@gmail.com>
2022-11-30 16:10:35 +0700

    feat: add create thread flow screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:fgd_flutter/create_thread_screen.dart';
 import 'package:fgd_flutter/login_screen.dart';
 import 'package:flutter/material.dart';
 
@@ -13,7 +14,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-      home: const LoginScreen(),
+      home: const CreateThread(),
     );
   }
 }
