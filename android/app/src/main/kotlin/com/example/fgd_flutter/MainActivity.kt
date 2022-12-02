31bc8f2124363797c05e79916c9f271626dee46b
Khaerul Latif <khaerullatif21@gmail.com>
2022-12-02 20:01:27 +0700

    feat: create post view widget

---
+++ android/app/src/main/kotlin/com/example/fgd_flutter/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.fgd_flutter
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/shared/charum_ui.dart
@@ -0,0 +1,5 @@
+library charum_ui;
+
+export 'app_colors.dart';
+export 'box_text.dart';
+export 'spacing.dart';
